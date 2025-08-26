a982fa9313832e76a53c9b4987df0fa072631575
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-08-27 01:17:26 +0600

    add calling fiture

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Heart Section
 const hearts = document.querySelectorAll(".fa-heart");
 const heartCounter = document.getElementById("heartCount");
 let count = 0;
@@ -9,6 +10,7 @@ hearts.forEach((heart) => {
   });
 });
 
+// Copy Button Section
 const copyBtns = document.querySelectorAll(".btn-outline");
 const hotlineNumbers = document.querySelectorAll(".hotlineNumber");
 let copyCount = document.getElementById("copyCounter");
@@ -21,5 +23,27 @@ copyBtns.forEach((copyBtn, index) => {
     alert(`The number has been copied. ${text}`);
     copyCounter++;
     copyCount.innerText = copyCounter;
+  });
+});
+
+// Call-Coin Section
+const phones = document.querySelectorAll(".btn-success");
+const coinCounter = document.getElementById("coin");
+const instituteName = document.querySelectorAll(".instituteName");
+let coin = 100;
+
+phones.forEach((phone, index) => {
+  phone.addEventListener("click", function () {
+    if (coin > 0) {
+      const iName = instituteName[index].innerHTML;
+      const number = hotlineNumbers[index].innerHTML;
+      alert(`Calling ${iName} ${number}...`);
+      coin -= 20;
+      console.log(coin);
+      coinCounter.innerHTML = coin;
+    } else {
+      console.log("You have 0 coin");
+      alert(`You need at least 20 coins to make a call.`);
+    }
   });
 });
